c6282051235cdd66aa68d3cf260e62a2cddacb91
Rosalvo Júnior <rosalvo@enesolucoes.com.br>
2019-09-16 16:01:20 -0300

    Autenticação JWT

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -9,7 +9,6 @@ class UserController {
     }
 
     const { id, name, email, provider } = await User.create(req.body);
-
     return res.json({ id, name, email, provider });
   }
 }
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '1ded4fb3f873b1c1cba297fac08c36d2',
+  expiresIn: '7d',
+};

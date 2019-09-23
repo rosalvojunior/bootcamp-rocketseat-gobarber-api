a12aa48db58296c5ea7137d2516949b4385531f9
Rosalvo Júnior <rosalvo@juniorbh.com.br>
2019-09-22 21:37:08 -0300

    Configurando templates de e-mail

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -116,8 +116,13 @@ class AppointmentController {
           model: User,
           as: 'provider',
           attributes: ['name', 'email'],
-        }
-      ]
+        },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
     });
 
     if (!appointment) {
@@ -147,7 +152,14 @@ class AppointmentController {
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'Agendamento cancelado',
-      text: 'Você tem um novo cancelamento',
+      template: 'cancelletion',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "dd 'de' MMMM', às' H:mm'h'", {
+          locale: pt,
+        }),
+      },
     });
 
     res.json(appointment);

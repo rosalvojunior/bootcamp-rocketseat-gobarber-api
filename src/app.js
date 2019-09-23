d9cdc88fc34a05934653b6f3807418334a8a6f47
Rosalvo Júnior <rosalvo@enesolucoes.com.br>
2019-09-23 10:19:33 -0300

    Tratamento de exceções

--- src/app.js
+++ src/app.js
@@ -1,15 +1,22 @@
 import express from 'express';
 import path from 'path';
+import * as Sentry from '@sentry/node';
+import Youch from 'youch';
+import 'express-async-errors';
 import routes from './routes';
 
+import sentryConfig from './config/sentry';
 import './database';
 
 class App {
   constructor() {
     this.server = express();
+    Sentry.init(sentryConfig);
+    this.server.use(Sentry.Handlers.requestHandler());
 
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
@@ -22,6 +29,19 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      // if (process.env.NODE_ENV === 'development') {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+      // }
+
+      // return res.status(500).json({ error: 'Internal server error' });
+    });
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dns: 'https://5aab756f21014d54afe41b1283a9ff95@sentry.io/1758388',
+};
